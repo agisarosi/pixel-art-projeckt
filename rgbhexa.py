17a56f9c0418df07801f013852d483e70c9a048d
Sárosi Ágnes <sarosiagnes48@gmail.com>
2024-05-06 12:34:46 +0200

    mehet

--- rgbhexa.py
+++ rgbhexa.py
@@ -1,15 +1,11 @@
-def rgb_to_hex(rgb_color):
-    # Check if the RGB values are within the valid range
-    for color in rgb_color:
-        if color < 0 or color > 255:
-            raise ValueError("RGB values must be between 0 and 255")
+def rgb_to_hex(rgb):
+    """Convert RGB tuple to hexadecimal color code."""
+    r, g, b = rgb
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
 
-    # Convert RGB to hexadecimal
-    hex_color = "#{:02X}{:02X}{:02X}".format(rgb_color[0], rgb_color[1], rgb_color[2])
 
-    return hex_color
-
-# Example usage
-rgb_color = (255, 69, 0)
+rgb_color = (255, 128, 0)
 hex_color = rgb_to_hex(rgb_color)
-print("Hexadecimal:", hex_color)
+print("RGB:", rgb_color)
+print("Hex:", hex_color)
+
